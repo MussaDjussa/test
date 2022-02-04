7484053e242365a062b819c0b688ebe310657427
superMussa <amushamid@mail.ru>
2022-02-04 19:02:24 +0300

    final commit

--- WeatherConsoleApplicaiton/Program.cs
+++ WeatherConsoleApplicaiton/Program.cs
@@ -8,35 +8,41 @@ namespace WeatherConsoleApplicaiton
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("enter your api: ");
-            //string api = Console.ReadLine();
-
-            //string getCiry = Console.ReadLine();
-
-            WebRequest webRequest = WebRequest.Create(@"https://api.openweathermap.org/data/2.5/weather?q=Kazan&APPID=ad65da604c31a382a6fc8b5478f3f08f");
-
-            webRequest.Method = "POST";
-            webRequest.ContentType = "application/x-www-urlencoded";
-
-            WebResponse webResponse = webRequest.GetResponse();
-
-            string answer = string.Empty;
-
-            using (Stream stream = webResponse.GetResponseStream())
+            while (true)
             {
-                using (StreamReader reader = new StreamReader(stream))
+                Console.WriteLine("Введите город, в котором хотите узнать погоду:");
+
+                string city = Console.ReadLine();
+
+                WebRequest webRequest = WebRequest.Create($@"https://api.openweathermap.org/data/2.5/weather?q={city}&APPID=ad65da604c31a382a6fc8b5478f3f08f");
+
+                webRequest.Method = "POST";
+                webRequest.ContentType = "application/x-www-urlencoded";
+
+                WebResponse webResponse = webRequest.GetResponse();
+
+                string answer = string.Empty;
+
+                using (Stream stream = webResponse.GetResponseStream())
                 {
-                    answer = reader.ReadToEnd();
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        answer = reader.ReadToEnd();
+                    }
                 }
+
+                webResponse.Close();
+
+                OpenWeather ow = JsonConvert.DeserializeObject<OpenWeather>(answer);
+
+                Console.WriteLine("Город: " + ow.Name);
+                Console.WriteLine("Температура: " + ow.main.Temperature);
+                Console.WriteLine("Давление: " + ow.main.Pressure);
+                Console.WriteLine("Влажность: " + ow.main.Humidity);
+                Console.WriteLine("Скорость ветра: " + ow.wind.Speed);
+                Console.WriteLine("Описание Погоды: " + ow.weather[0].Description);
+                Console.WriteLine();
             }
-
-            webResponse.Close();
-
-            OpenWeather ow = JsonConvert.DeserializeObject<OpenWeather>(answer);
-
-            Console.WriteLine(ow.weather[0].Main);
-            Console.WriteLine(ow.weather[0].Description);
-            Console.WriteLine(ow.Name);
         }
     }
 }
